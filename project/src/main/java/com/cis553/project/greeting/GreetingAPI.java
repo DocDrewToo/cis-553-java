6f0ceb7efbefc246b3aa126f3d3f8b43b588e5ae
Doc Drew <docdrew@umich.edu>
2021-11-03 17:15:20 -0400

    Adding API endpoints"

---
+++ project/src/main/java/com/cis553/project/greeting/GreetingAPI.java
@@ -0,0 +1,18 @@
+package com.cis553.project.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingAPI {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public GreetingObject greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new GreetingObject(counter.incrementAndGet(), String.format(template, name));
+    }
+}
